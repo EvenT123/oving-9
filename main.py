80b89a156036d28e0c289fec7a1351028e0a7ec3
EvenTinjar <eventinjar@live.no>
2021-10-28 19:10:34 +0200

    legger til noe

--- main.py
+++ main.py
@@ -4,29 +4,22 @@ class Flervalgspørsmål:
         self.y = svaralternativ
         self.z = riktigSvar
 
-    def sjekk_svar(self, svar, fasit):
-        self.f = svar
-        if self.f == fasit + 1:
-            print("Riktig!")
-        else:
-            print("Feil!")
     def __str__(self):
         return f"{self.x} \n1: {self.y[0]} \n2: {self.y[1]} \n3: {self.y[2]}"
 
-spørsmåltekst1 = "\nHvilken bil er fra italia?"
-svarAlternativ1 = ["Ferari", "Audi", "Mazda"]
-riktigSvar1 = 0
+    def sjekk_svar(self, svar):
+        self.f = svar
+        if self.f == riktigSvar + 1:
+            print("Riktig!")
+        else:
+            print("Feil!")
 
-spørsmåltekst2 = "\nHvilken skole er best?"
-svarAlternativ2 = ["UIO", "UIS", "OsloMet"]
-riktigSvar2 = 1
 
-spm1 = Flervalgspørsmål(spørsmåltekst1, svarAlternativ1, riktigSvar1)
-print(str(spm1))
-brukerSvar1 = int(input("Hvilket svar er riktig?: "))
-spm1.sjekk_svar(brukerSvar1, riktigSvar1)
+spørsmåltekst = "Hvilken bil er fra italia?"
+svarAlternativ = ["Ferari", "Audi", "Mazda"]
+riktigSvar = 0
 
-spm2 = Flervalgspørsmål(spørsmåltekst2, svarAlternativ2, riktigSvar2)
-print(str(spm2))
-brukerSvar2 = int(input("Hvilket svar er riktig?: "))
-spm1.sjekk_svar(brukerSvar2, riktigSvar2)
+x = Flervalgspørsmål(spørsmåltekst, svarAlternativ, riktigSvar)
+print(str(x))
+brukerSvar = int(input("Hvilket svar er riktig?: "))
+x.sjekk_svar(brukerSvar)

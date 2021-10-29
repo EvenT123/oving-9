ae5b7dc8ee23111865e13cb7702e5b70fb98e8d8
EvenTinjar <eventinjar@live.no>
2021-10-29 12:18:57 +0200

    legger til oving 8 b

---
+++ opgB.py
@@ -0,0 +1,29 @@
+
+    class Flervalgspørsmål:
+        def __init__(self, spørsmåltekst, svaralternativ, riktigSvar):
+            self.x = spørsmåltekst
+            self.y = svaralternativ
+            self.z = riktigSvar
+
+        def __str__(self):
+            return f"{self.x} \n1: {self.y[0]} \n2: {self.y[1]} \n3: {self.y[2]}"
+
+        def sjekk_svar(self, svar):
+            self.f = svar
+            if self.f == riktigSvar + 1:
+                print("Riktig!")
+            else:
+                print("Feil!")
+    return Flervalgspørsmål
+
+return Flervalgspørsmål = new_func()
+
+
+spørsmåltekst = "Hvilken bil er fra italia?"
+svarAlternativ = ["Ferari", "Audi", "Mazda"]
+riktigSvar = 0
+
+x = Flervalgspørsmål(spørsmåltekst, svarAlternativ, riktigSvar)
+print(str(x))
+brukerSvar = int(input("Hvilket svar er riktig?: "))
+x.sjekk_svar(brukerSvar)
